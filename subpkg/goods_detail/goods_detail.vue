cb8870cec032488ed1eb7d16de521a68ddcb09c3
liangyuanyong <2797834006@qq.com>
2023-10-20 11:50:48 +0800

    首页功能开发完成

---
+++ subpkg/goods_detail/goods_detail.vue
@@ -0,0 +1,19 @@
+<template>
+  <view>
+    1111
+  </view>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        
+      };
+    }
+  }
+</script>
+
+<style lang="scss">
+
+</style>
